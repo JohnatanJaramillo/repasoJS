8e988395e85f8bfcb8676de17e56e496a28a8ae5
JohnatanJaramillo <jejo9202@gmail.com>
2023-03-25 10:13:08 -0500

    Primer commit

---
+++ baseDatos.js
@@ -0,0 +1,17 @@
+export let productos = [
+    {
+        nombre:"camiseta 1", 
+        precio:80000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojejo.appspot.com/o/foto2.webp?alt=media&token=777e85ac-8191-4f18-ad67-92cd2ea78f5a"
+    },
+    {
+        nombre:"chaqueta 1", 
+        precio:180000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojejo.appspot.com/o/foto1.webp?alt=media&token=1af42074-cc2a-47f3-a89e-c566f5fe555e"
+    },
+    {
+        nombre:"zapatos 1", 
+        precio:380000,
+        foto:"https://firebasestorage.googleapis.com/v0/b/tiendasabadojejo.appspot.com/o/foto3.webp?alt=media&token=13147f43-6cda-4e1c-b2cd-af92c0d17164"
+    }
+]
---
+++ controlador.js
@@ -0,0 +1,21 @@
+import { productos } from "./baseDatos.js";
+
+let fila = document.getElementById("fila");
+
+//Recorriendo un arreglo
+productos.forEach(function(producto){
+    let columna = document.createElement("div");
+    columna.classList.add("col");
+
+    let tarjeta = document.createElement("div");
+    tarjeta.classList.add("card","shadow");
+
+    let foto = document.createElement("img");
+    foto.classList.add("img-fluid");
+    foto.src = producto.foto;
+
+    //Padre e hijos
+    tarjeta.appendChild(foto);
+    columna.appendChild(tarjeta);
+    fila.appendChild(columna);
+});
